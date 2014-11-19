05ebfd294278c05bbe25f02945041aadbd1e2efe
Vyacheslav Valyavskiy <vvalyavskiy@mirantis.com>
2014-11-19 20:36:19 +0400

    Revert 'fix bug when all networks are assigned to the admin interface'
    This reverts commit 150a970ba86af4f1fb9e6991c2ce0c7b1444e418.
    
    Change-Id: I9c232a5d7fb300c2aa9f70cbdf7b6435b983bef9

--- nailgun/nailgun/objects/node.py
+++ nailgun/nailgun/objects/node.py
@@ -425,7 +425,6 @@ class Node(NailgunObject):
             cls.update_interfaces(instance)
 
         cluster_changed = False
-        added_to_new_cluster = False
         if "cluster_id" in data:
             new_cluster_id = data.pop("cluster_id")
             if instance.cluster_id:
@@ -443,7 +442,6 @@ class Node(NailgunObject):
                 if new_cluster_id is not None:
                     # assigning node to cluster
                     cluster_changed = True
-                    added_to_new_cluster = True
                     cls.add_into_cluster(instance, new_cluster_id)
 
         # calculating flags
@@ -463,7 +461,7 @@ class Node(NailgunObject):
             cls.update_pending_roles(instance, pending_roles)
 
         # updating interfaces assignment
-        if added_to_new_cluster:
+        if instance.cluster:
             cls.update_node_interface_mapping(instance)
 
         if any((
