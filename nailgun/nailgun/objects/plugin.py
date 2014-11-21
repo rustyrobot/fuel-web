8411d52b87db1c7051145d9e616b2b9cc9ea5922
Evgeniy L <eli@mirantis.com>
2014-11-21 21:25:16 +0400

    Plugins multiversioning
    Change-Id: I51ba03166cd2a7379c6f0912d668265bf88a316d

--- nailgun/nailgun/objects/plugin.py
+++ nailgun/nailgun/objects/plugin.py
@@ -14,11 +14,13 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from distutils.version import LooseVersion
+from itertools import groupby
+
+from nailgun.db import db
 from nailgun.db.sqlalchemy.models import plugins as plugin_db_model
 from nailgun.objects import base
 from nailgun.objects.serializers import plugin
-
-from nailgun.db import db
 
 
 class Plugin(base.NailgunObject):
@@ -35,3 +37,20 @@ class Plugin(base.NailgunObject):
 class PluginCollection(base.NailgunCollection):
 
     single = Plugin
+
+    @classmethod
+    def all_newest(cls):
+        sorted_by_version = sorted(
+            cls.all(),
+            key=lambda p: LooseVersion(p.version),
+            reverse=True)
+
+        plugins = []
+        grouped_by_name = groupby(sorted_by_version, lambda p: p.name)
+        for name, plugin in grouped_by_name:
+            # The first element in the list is a plugin
+            # with highest version
+            first_plugin = plugin.next()
+            plugins.append(first_plugin)
+
+        return plugins
